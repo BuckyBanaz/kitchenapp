1245d8cd60802e2bad22a1108e0ca349a142c0d3
Parikshit Verma <114131682+BuckyBanaz@users.noreply.github.com>
2025-09-28 12:47:02 +0530

    toast_utils.dart

--- lib/utils/toast_utils.dart
+++ lib/utils/toast_utils.dart
@@ -1,13 +1,1 @@
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:flutter/material.dart';
 
-void showToast(String msg) {
-  Fluttertoast.showToast(
-    msg: msg,
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    backgroundColor: Colors.black87,
-    textColor: Colors.white,
-    fontSize: 14,
-  );
-}
